bfcf32faa8051434bf8745e06e2d636b365081d0
Allan Menoret <allan.menoret@gmail.com>
2019-10-14 15:49:08 +0200

    init with template and docker file

---
+++ poc_azure_blazor/ForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using poc_azure_blazor.Data;
+
+namespace poc_azure_blazor
+{
+    [Route("api/[controller]")]
+    [Controller]
+    public class ForecastController : Controller
+    {
+        private readonly WeatherForecastService weatherForecastService;
+
+        public ForecastController(WeatherForecastService weatherForecastService)
+        {
+            this.weatherForecastService = weatherForecastService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> GetAsync()
+        {
+            var test = await this.weatherForecastService.GetForecastAsync(DateTime.Now);
+
+            return test;
+        }
+    }
+}
